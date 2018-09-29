13e524fadbdb9e8488a4398145b51a7bbb9d9a74
Snezhana <snowwe25@rambler.ru>
2018-09-30 01:13:45 +0300

    with_same_brackets_but_question_in_rightBrackets

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ module.exports = function check(str, bracketsConfig) {
     brConfig = Array.from(brConfig);
     let leftBrackets = []
         , rightBrackets = []
-        , sameBrackets = []
+        , sameBrackets = [] //if open and close bracket is same
         , tempArr = []
-        , index = 0;
+        , index = -1;
 
     for (let i = 0; i <= brConfig.length - 1; i += 2) {
         if (brConfig[i] === brConfig[i + 1]) {
@@ -19,32 +19,36 @@ module.exports = function check(str, bracketsConfig) {
         }
     }
 
-    for (let i = 0; i <= str.length; ++i) {
+    for (let i = 0; i < str.length; ++i) {
 //if same brackets
-        for (let j = 0; j <= sameBrackets.length; ++j) {
+        for (let j = 0; j < sameBrackets.length; ++j) {
             if (str[i] === sameBrackets[j]) {
-
+                if (tempArr.length === 0) {
+                    tempArr.push(str[i]);
+                }
+                else if (tempArr[tempArr.length - 1] === sameBrackets[j]) {
+                    tempArr.pop(str[i]);
+                }
+                else {
+                    tempArr.push(str[i]);
+                }
             }
         }
 //else
-        for (let j = 0; j <= leftBrackets.length; ++j) {
+        for (let j = 0; j < leftBrackets.length; ++j) {
             if (str[i] === leftBrackets[j]) {
                 tempArr.push(str[i]);
                 index = j;
             } else {
-                for (let j = 0; j <= rightBrackets.length; ++j) {
-                    if (str[i] === rightBrackets[j]
-                        && tempArr.length > 0) {
-                        if (index === j) {
+                for (let j = 0; j < rightBrackets.length; ++j) {
+                    if (str[i] === rightBrackets[j]) {
+                        if (index === j && tempArr.length > 0) {
                             tempArr.pop(str[i]);
-                        } else {
-                            return false;
                         }
                     }
                 }
             }
         }
     }
-
-return tempArr.length !== 0;
+    return tempArr.length === 0;
 };
