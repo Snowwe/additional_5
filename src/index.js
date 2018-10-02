6e938b4fcaba92fbcec08a9eee94970ec3676b99
Snezhana <snowwe25@rambler.ru>
2018-10-02 22:11:03 +0300

    version_3

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-    let tempArr = [];
-    for (let i = 0; i < str.length; i++) {
-        for (let j = 0; j < bracketsConfig.length; j++) {
-            if (bracketsConfig[j][1] === str[i] &&
-                bracketsConfig[j][0] === tempArr[tempArr.length - 1]) {
-                tempArr.pop();
-            } else if (bracketsConfig[j][0] === str[i]) {
-                tempArr.push(str[i]);
-            } else if (bracketsConfig[j][1] === str[i]) {
-                return false;
+
+    for (let strKey = str.length; strKey >= 0; strKey--) {
+        for (let bracketKey = 0; bracketKey < bracketsConfig.length; bracketKey++) {
+            if (str[strKey] === bracketsConfig[bracketKey][0]) {
+                str = str.replace(bracketsConfig[bracketKey].join(''), '');
             }
         }
     }
-    return tempArr.length === 0;
+    return !str.length;
 };
